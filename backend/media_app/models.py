f75d2877f8b0c8338f67f8e2c9ef0d818167513f
T-supernatural <tinuademichael@gmail.com>
2025-07-24 20:23:39 +0100

    ADD CHANGES

--- backend/media_app/models.py
+++ backend/media_app/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Media(models.Model):
+    MEDIA_TYPES = [
+        ('audio', 'Audio'),
+        ('video', 'Video'),
+        ('photo', 'Photo'),
+        ('article', 'Article'),
+    ]
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    description = models.TextField()
+    media_type = models.CharField(max_length=10, choices=MEDIA_TYPES)
+    file = models.FileField(upload_to='media_files/', blank=True, null=True)
+    thumbnail = models.ImageField(
+        upload_to='thumbnails/', blank=True, null=True)
+    article_content = models.TextField(blank=True, null=True)
+
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    tags = models.CharField(max_length=250, blank=True,
+                            help_text="Comma-separated tags")
+
+    publish_date = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Media, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
